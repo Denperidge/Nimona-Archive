98150b801df10c8d4d01816548d64d6338cb8e17
Denperidge <denperidge@gmail.com>
2023-07-28 12:51:29 +0200

    Fixed incorrect page order

--- pug.config.js
+++ pug.config.js
@@ -1,16 +1,32 @@
 const fs = require("fs");
 
+const nonNumbers = /[^\d]+/; 
+function getNumbers(string) {
+    return parseFloat(string.replace(nonNumbers, ""));
+}
+
+function sortByNumbersInTheName(a, b) {
+    if (getNumbers(a) < getNumbers(b)) {
+        return -1;
+    } else if (getNumbers(a) > getNumbers(b)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
 let chapters = {};
 fs.readdirSync("extracted/").forEach((chapterName) => {
     const chapterDir = "extracted/" + chapterName;
-    const chapterNumber = chapterName.replace(/[^\d]+/, "");
+    const chapterNumber = getNumbers(chapterName);
 
     let chapter = [];
 
-    fs.readdirSync(chapterDir).forEach((fileName) => {
+    fs.readdirSync(chapterDir).sort(sortByNumbersInTheName).forEach((fileName) => {
         if (!fileName.endsWith(".json")) return;
-        const jsonPath = chapterDir + "/" + fileName;
 
+        const jsonPath = chapterDir + "/" + fileName;
         const pageData = JSON.parse(fs.readFileSync(jsonPath, {encoding: "utf-8"}));
         const base64 = fs.readFileSync(jsonPath.replace(".json", ".jpg"), "base64");
 
