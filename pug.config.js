29ad905bb5a11cdb725984f02393c1a3049fba69
Denperidge <denperidge@gmail.com>
2023-07-28 12:02:04 +0200

    Initial Commit (chapter 1 succesful basic load)

---
+++ pug.config.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+let chapters = []
+fs.readdirSync("extracted/").forEach((chapterName) => {
+    const chapterDir = "extracted/" + chapterName;
+
+    let chapter = [];
+
+    fs.readdirSync(chapterDir).forEach((fileName) => {
+        if (!fileName.endsWith(".json")) return;
+        const jsonPath = chapterDir + "/" + fileName;
+
+        const pageData = JSON.parse(fs.readFileSync(jsonPath, {encoding: "utf-8"}));
+        const base64 = fs.readFileSync(jsonPath.replace(".json", ".jpg"), "base64");
+
+        pageData.image = `data:image/jpg;base64,${base64}`;
+
+        chapter.push(pageData);
+    });
+
+    chapters.push(chapter);
+    
+    
+});
+
+
+
+module.exports = {
+    locals: {
+        chapters: chapters
+    }
+}
