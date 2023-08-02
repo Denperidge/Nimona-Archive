f6da183bc859d29881d60b94e491e3b2947390ba
Denperidge <denperidge@gmail.com>
2023-08-02 18:26:45 +0200

    Swapped animate button text

--- src/themes/background.js
+++ src/themes/background.js
@@ -3,11 +3,10 @@ const animateButton = document.getElementById("animate");
 function animate(e) {
     const background = document.getElementById("background");
     if (background.paused) {
-        animateButton.innerHTML = "Enable animations";
         background.play();
-
-    } else {
         animateButton.innerHTML = "Disable animations";
+    } else {
+        animateButton.innerHTML = "Enable animations";
         background.pause()
     }
     
