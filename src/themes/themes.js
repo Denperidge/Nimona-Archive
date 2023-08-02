b6e819cd43acb570938bccacac1cedc697c16b3f
Denperidge <denperidge@gmail.com>
2023-08-02 17:26:21 +0200

    Created theme switch functions

---
+++ src/themes/themes.js
@@ -0,0 +1,22 @@
+let themeElements = document.getElementsByClassName("theme");
+let themes = [];
+
+for (let i = 0; i < themeElements.length; i++) {
+    themes.push(themeElements[i].id);
+}
+
+function applyTheme(enabledTheme) {
+    for (let i = 0; i < themes.length; i++) {
+        let theme = themes[i];
+        if (theme == enabledTheme) {
+            disableTheme(theme, false);
+        } else {
+            disableTheme(theme, true);
+        }
+    }
+}
+
+function disableTheme(id, disable=true) {
+    document.getElementById(id).disabled = disable; //("disabled", disable)
+}
+
